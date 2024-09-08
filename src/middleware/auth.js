7435e6b8a537a3286099937806081058b7214510
Meet4009 <meetsolanki4009@gmail.com>
2024-09-08 14:58:23 +0530

    change error code in isAuthenticatedUser

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,7 +8,7 @@ exports.isAuthenticatedUser = async (req, res, next) => {
     try {
         const { token } = req.cookies;
         if (!token) {
-            return res.status(404).json({
+            return res.status(401).json({
                 status: false,
                 data: {},
                 message: "pleses login to access this resource",
