b08e6ec78d1bddfb37a946fc87ee2ea19b6dfe4a
Meet4009 <meetsolanki4009@gmail.com>
2024-12-23 12:41:15 +0530

    add local Database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
+// const connectdatabase = () => {
+//     mongoose.connect(process.env.DB_URI).then((data) => {
+//         console.log(`mongoDB connected with server: ${data.connection.host}`);
+//     })
+// }
+
+
 const connectdatabase = () => {
-    mongoose.connect(process.env.DB_URI).then((data) => {
+    mongoose.connect(process.env.DB_URL).then((data) => {
         console.log(`mongoDB connected with server: ${data.connection.host}`);
     })
 }
