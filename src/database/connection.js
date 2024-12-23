17d354378264747254a7ced71582064aa761113b
Meet4009 <meetsolanki4009@gmail.com>
2024-12-23 14:14:30 +0530

    db uri changes

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,14 +1,7 @@
 const mongoose = require("mongoose");
 
-// const connectdatabase = () => {
-//     mongoose.connect(process.env.DB_URI).then((data) => {
-//         console.log(`mongoDB connected with server: ${data.connection.host}`);
-//     })
-// }
-
-
 const connectdatabase = () => {
-    mongoose.connect(process.env.DB_URL).then((data) => {
+    mongoose.connect(process.env.DB_URI).then((data) => {
         console.log(`mongoDB connected with server: ${data.connection.host}`);
     })
 }
