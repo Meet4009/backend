77ae5dc25dbcca547c290862124d31bf2f06df8c
Meet4009 <meetsolanki4009@gmail.com>
2025-01-20 11:40:51 +0530

    add dbconnction function with DB_URL

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require("mongoose");
 
+// const connectdatabase = () => {
+//     mongoose.connect(process.env.DB_URL).then((data) => {
+//         console.log(`mongoDB connected with server: ${data.connection.host}`);
+//     })
+// }
+
 const connectdatabase = () => {
     mongoose.connect(process.env.DB_URI).then((data) => {
         console.log(`mongoDB connected with server: ${data.connection.host}`);
